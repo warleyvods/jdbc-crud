6dc99296c11b38e125087cc875f156adee747b2f
Vinicius Augusto <vinicius_augusto_@msn.com>
2021-06-02 22:24:14 -0300

    Implemented Retrieve function

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT id, name FROM seller");
+
+            while(rs.next()) {
+                System.out.println(rs.getInt("id") + ", " + rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+    }
+}
